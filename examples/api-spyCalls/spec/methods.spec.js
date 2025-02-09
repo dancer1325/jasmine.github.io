0237150ecbe6950533f830f7fb792f6e5db549b8
dancer1325 <alfredotic0809@gmail.com>
2025-02-09 15:25:43 +0100

    doc(api.spyCalls): clear notes and add examples

--- examples/api-spyCalls/spec/methods.spec.js
+++ examples/api-spyCalls/spec/methods.spec.js
@@ -12,4 +12,17 @@ describe('methods', () => {
     });
 
     // TODO:
+
+    it('mostRecent()', () => {
+        const spy = jasmine.createSpy('spy');
+
+        // 1. BEFORE invoking       -> undefined
+        console.log("BEFORE INVOKING - spy.calls.mostRecent() ", spy.calls.mostRecent());     // -- via -- .calls
+        spy(1, 2, 3);
+        spy('a', 'b', 'c');
+        // 2. AFTER invoking        -> recent callData
+        console.log("AFTER INVOKING - spy.calls.mostRecent() ", spy.calls.mostRecent());     // -- via -- .calls
+    });
+
+    // TODO:
 })
